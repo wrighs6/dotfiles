897503f7660b44ce3420ce2af03094d2b287f179
wrighs6 <wrighs6@rpi.edu>
2023-04-16 20:10:17 -0400

    switched back to unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "wrighs6's NixOS config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
